6125af66492f540dd233d1ae007b6915ba3a8942
José Ramón Blanco <58235013+jrcosio@users.noreply.github.com>
2021-06-11 01:20:17 +0200

    Add files via upload

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author jrblanco
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MainCalculadora ventana = new MainCalculadora();
+        ventana.setVisible(true);
+        ventana.setTitle("Calculadora v1.0");
+    }
+    
+}
