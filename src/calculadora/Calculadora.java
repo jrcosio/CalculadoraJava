998a1855de36871498fbf11f0a1e5523ae75aac7
José Ramón Blanco <58235013+jrcosio@users.noreply.github.com>
2021-06-11 23:30:56 +0200

    Add files via upload

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -16,10 +16,36 @@ public class Calculadora {
      */
     public static void main(String[] args) {
         
+       
+        
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
         MainCalculadora ventana = new MainCalculadora();
         ventana.setVisible(true);
         ventana.setLocationRelativeTo(null);
         ventana.setTitle("Calculadora v1.0");
+        /* Create and display the form */
+       // java.awt.EventQueue.invokeLater(new Runnable() {
+       //     public void run() {
+       //         new winmain2().setVisible(true);
+       //     }
+      //  });
     }
     
 }
